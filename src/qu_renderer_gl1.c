827a9b6977a0b6cb2fac2f6ada9193f0bdab0008
kelbeppin <135161173+kelbeppin@users.noreply.github.com>
2023-09-02 02:02:05 +0900

    Complete [renderer-gl1].

--- src/qu_renderer_gl1.c
+++ src/qu_renderer_gl1.c
@@ -109,7 +109,21 @@ static GLenum const texture_format_map[4] = {
 struct qu__gl1_renderer_priv
 {
     GLuint bound_texture;
+    GLuint bound_surface;
     float const *vertex_data[QU__TOTAL_VERTEX_FORMATS];
+    int w_display;
+    int h_display;
+
+    PFNGLBINDFRAMEBUFFEREXTPROC glBindFramebufferEXT;
+    PFNGLBINDRENDERBUFFEREXTPROC glBindRenderbufferEXT;
+    PFNGLCHECKFRAMEBUFFERSTATUSEXTPROC glCheckFramebufferStatusEXT;
+    PFNGLDELETEFRAMEBUFFERSEXTPROC glDeleteFramebuffersEXT;
+    PFNGLDELETERENDERBUFFERSEXTPROC glDeleteRenderbuffersEXT;
+    PFNGLGENFRAMEBUFFERSEXTPROC glGenFramebuffersEXT;
+    PFNGLGENRENDERBUFFERSEXTPROC glGenRenderbuffersEXT;
+    PFNGLFRAMEBUFFERRENDERBUFFEREXTPROC glFramebufferRenderbufferEXT;
+    PFNGLFRAMEBUFFERTEXTURE2DEXTPROC glFramebufferTexture2DEXT;
+    PFNGLRENDERBUFFERSTORAGEEXTPROC glRenderbufferStorageEXT;
 };
 
 static struct qu__gl1_renderer_priv priv;
@@ -124,11 +138,50 @@ static void color_conv(GLfloat *dst, qu_color color)
     dst[3] = ((color >> 0x18) & 0xFF) / 255.f;
 }
 
+static bool check_glext(char const *extension)
+{
+    char *extensions = qu_strdup((char const *) glGetString(GL_EXTENSIONS));
+    char *token = strtok(extensions, " ");
+    bool found = false;
+
+    while (token) {
+        if (strcmp(token, extension) == 0) {
+            found = true;
+            break;
+        }
+
+        token = strtok(NULL, " ");
+    }
+
+    free(extensions);
+
+    return found;
+}
+
+static void load_gl_functions(void)
+{
+    priv.glBindFramebufferEXT = qu__core_get_gl_proc_address("glBindFramebufferEXT");
+    priv.glBindRenderbufferEXT = qu__core_get_gl_proc_address("glBindRenderbufferEXT");
+    priv.glCheckFramebufferStatusEXT = qu__core_get_gl_proc_address("glCheckFramebufferStatusEXT");
+    priv.glDeleteFramebuffersEXT = qu__core_get_gl_proc_address("glDeleteFramebuffersEXT");
+    priv.glDeleteRenderbuffersEXT = qu__core_get_gl_proc_address("glDeleteRenderbuffersEXT");
+    priv.glGenFramebuffersEXT = qu__core_get_gl_proc_address("glGenFramebuffersEXT");
+    priv.glGenRenderbuffersEXT = qu__core_get_gl_proc_address("glGenRenderbuffersEXT");
+    priv.glFramebufferRenderbufferEXT = qu__core_get_gl_proc_address("glFramebufferRenderbufferEXT");
+    priv.glFramebufferTexture2DEXT = qu__core_get_gl_proc_address("glFramebufferTexture2DEXT");
+    priv.glRenderbufferStorageEXT = qu__core_get_gl_proc_address("glRenderbufferStorageEXT");
+}
+
 //------------------------------------------------------------------------------
 
 static bool gl1__query(qu_params const *params)
 {
     if (qu__core_get_renderer() != QU__RENDERER_GL_COMPAT) {
+        return false;
+    }
+
+    if (!check_glext("GL_EXT_framebuffer_object")) {
+        QU_ERROR("Required OpenGL extension GL_EXT_framebuffer_object is not supported.\n");
         return false;
     }
 
@@ -138,6 +191,8 @@ static bool gl1__query(qu_params const *params)
 static void gl1__initialize(qu_params const *params)
 {
     memset(&priv, 0, sizeof(priv));
+
+    load_gl_functions();
 
     _GL_CHECK(glEnable(GL_TEXTURE_2D));
     _GL_CHECK(glEnable(GL_BLEND));
@@ -170,6 +225,19 @@ static void gl1__apply_projection(qu_mat4 const *projection)
 static void gl1__apply_transform(qu_mat4 const *transform)
 {
     glLoadMatrixf(transform->m);
+}
+
+static void gl1__apply_surface(struct qu__texture_data const *texture)
+{
+    if (texture) {
+        _GL_CHECK(priv.glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, texture->priv[1]));
+        _GL_CHECK(glViewport(0, 0, texture->image.width, texture->image.height));
+        priv.bound_surface = texture->priv[1];
+    } else {
+        _GL_CHECK(priv.glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0));
+        _GL_CHECK(glViewport(0, 0, priv.w_display, priv.h_display));
+        priv.bound_surface = 0;
+    }
 }
 
 static void gl1__apply_texture(struct qu__texture_data const *data)
@@ -220,6 +288,9 @@ static void gl1__apply_vertex_format(enum qu__vertex_format vertex_format)
 static void gl1__exec_resize(int width, int height)
 {
     _GL_CHECK(glViewport(0, 0, width, height));
+
+    priv.w_display = width;
+    priv.h_display = height;
 }
 
 static void gl1__exec_clear(void)
@@ -268,6 +339,80 @@ static void gl1__unload_texture(struct qu__texture_data *texture)
     _GL_CHECK(glDeleteTextures(1, &id));
 }
 
+static void gl1__set_texture_smooth(struct qu__texture_data *texture, bool smooth)
+{
+    GLuint id = (GLuint) texture->priv[0];
+
+    if (!id) {
+        return;
+    }
+
+    _GL_CHECK(glBindTexture(GL_TEXTURE_2D, id));
+
+    if (smooth) {
+        _GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
+    } else {
+        _GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
+    }
+
+    _GL_CHECK(glBindTexture(GL_TEXTURE_2D, priv.bound_texture));
+}
+
+static void gl1__create_surface(struct qu__texture_data *texture)
+{
+    GLuint fbo;
+    GLuint depth;
+    GLuint color;
+
+    _GL_CHECK(priv.glGenFramebuffersEXT(1, &fbo));
+    _GL_CHECK(priv.glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo));
+
+    _GL_CHECK(priv.glGenRenderbuffersEXT(1, &depth));
+    _GL_CHECK(priv.glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, depth));
+    _GL_CHECK(priv.glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, GL_DEPTH_COMPONENT16,
+                                            texture->image.width, texture->image.height));
+
+    _GL_CHECK(glGenTextures(1, &color));
+    _GL_CHECK(glBindTexture(GL_TEXTURE_2D, color));
+    _GL_CHECK(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, texture->image.width, texture->image.height,
+                           0, GL_RGBA, GL_UNSIGNED_BYTE, NULL));
+
+    _GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
+    _GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+
+    _GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
+    _GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
+
+    _GL_CHECK(priv.glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, GL_DEPTH_ATTACHMENT_EXT,
+                                                GL_RENDERBUFFER_EXT, depth));
+    _GL_CHECK(priv.glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT,
+                                             GL_TEXTURE_2D, color, 0));
+
+    GLenum status = priv.glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT);
+
+    if (status != GL_FRAMEBUFFER_COMPLETE_EXT) {
+        QU_HALT("[TODO] FBO error handling.");
+    }
+
+    texture->priv[0] = color;
+    texture->priv[1] = fbo;
+    texture->priv[2] = depth;
+
+    _GL_CHECK(priv.glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, priv.bound_surface));
+    _GL_CHECK(glBindTexture(GL_TEXTURE_2D, priv.bound_texture));
+}
+
+static void gl1__destroy_surface(struct qu__texture_data *texture)
+{
+    GLuint color = texture->priv[0];
+    GLuint fbo = texture->priv[1];
+    GLuint depth = texture->priv[2];
+
+    _GL_CHECK(priv.glDeleteFramebuffersEXT(1, &fbo));
+    _GL_CHECK(priv.glDeleteRenderbuffersEXT(1, &depth));
+    _GL_CHECK(glDeleteTextures(1, &color));
+}
+
 //------------------------------------------------------------------------------
 
 struct qu__renderer_impl const qu__renderer_gl1 = {
@@ -277,6 +422,7 @@ struct qu__renderer_impl const qu__renderer_gl1 = {
     .upload_vertex_data = gl1__upload_vertex_data,
     .apply_projection = gl1__apply_projection,
     .apply_transform = gl1__apply_transform,
+    .apply_surface = gl1__apply_surface,
     .apply_texture = gl1__apply_texture,
     .apply_clear_color = gl1__apply_clear_color,
     .apply_draw_color = gl1__apply_draw_color,
@@ -286,4 +432,7 @@ struct qu__renderer_impl const qu__renderer_gl1 = {
 	.exec_draw = gl1__exec_draw,
     .load_texture = gl1__load_texture,
     .unload_texture = gl1__unload_texture,
+    .set_texture_smooth = gl1__set_texture_smooth,
+    .create_surface = gl1__create_surface,
+    .destroy_surface = gl1__destroy_surface,
 };
